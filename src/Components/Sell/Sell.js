8890481c4d2682c8a85a329bb16131788a4c8fbf
First Last <salmanashraf853@gmail.com>
2019-11-06 20:26:32 +0100

    building sales page

--- src/Components/Sell/Sell.js
+++ src/Components/Sell/Sell.js
@@ -5,9 +5,11 @@ const Sell = () => {
     return (
         <div className="sell-page">
             <input type="file" className="product_img" />
-            <input placeholder="Enter product's name" className="product_name"/>
             <input placeholder="Enter your name" className="seller_name"/>
+            <input placeholder="Enter product's name" className="product_name"/>
             <input placeholder="Enter product's price" className="product_price"/>
+            <textarea placeholder="Item details"/>
+            <button>SELL ITEM</button>
         </div>
     );
 };
