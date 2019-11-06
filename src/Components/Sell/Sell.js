c0da9aa3f4362e73d9b2d563987eed4af6854749
First Last <salmanashraf853@gmail.com>
2019-11-06 20:05:08 +0100

    building sales page

--- src/Components/Sell/Sell.js
+++ src/Components/Sell/Sell.js
@@ -4,11 +4,10 @@ import './sell.css'
 const Sell = () => {
     return (
         <div className="sell-page">
+            <input type="file" className="product_img" />
             <input placeholder="Enter product's name" className="product_name"/>
             <input placeholder="Enter your name" className="seller_name"/>
             <input placeholder="Enter product's price" className="product_price"/>
-            <input placeholder="Enter product's name" className="product_name"/>
-            <input placeholder="Enter product's name" className="product_name"/>
         </div>
     );
 };
