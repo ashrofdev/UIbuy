610af18466b8d74ec781dbad95770cb3b56ae2ed
First Last <salmanashraf853@gmail.com>
2019-11-09 09:42:49 +0100

    Updates

--- precache-manifest.9c3c60a5d20907ba2f255069ec3315e3.js
+++ precache-manifest.9c3c60a5d20907ba2f255069ec3315e3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53977ec04d4bb37799f1fb5e38708d85",
+    "revision": "ea9139173e97d476755aabcd469d6295",
     "url": "/UIbuy/index.html"
   },
   {
-    "revision": "b06c9b7ddb014a6429c0",
-    "url": "/UIbuy/static/css/main.8c782419.chunk.css"
+    "revision": "d1c0523aff7e1fc137ab",
+    "url": "/UIbuy/static/css/main.3ab30d58.chunk.css"
   },
   {
     "revision": "4605eb61733859eb0143",
     "url": "/UIbuy/static/js/2.2e76c133.chunk.js"
   },
   {
-    "revision": "b06c9b7ddb014a6429c0",
-    "url": "/UIbuy/static/js/main.bf76e4d5.chunk.js"
+    "revision": "d1c0523aff7e1fc137ab",
+    "url": "/UIbuy/static/js/main.df020c31.chunk.js"
   },
   {
     "revision": "412c8bda13ac9653804a",
